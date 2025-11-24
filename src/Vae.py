58ca79245c1287f4cd7808550b69ab0a8980f22e
Leonardo Sani <leonardo.sani.pro@gmail.com>
2025-11-24 11:56:42 +0100

    fix import moduls problem

--- src/Vae.py
+++ src/Vae.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import copy
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
 
 # -----------------------------
 # Encoder
